0a8bdf22440562632c9c7f23e40d2da5b8b6794d
Douglas <douglaspfeifer@gmail.com>
2021-09-18 19:41:55 -0300

    Add food strings

---
+++ iOS/CookingRecipeApp/Strings.swift
@@ -0,0 +1,136 @@
+//
+//  Strings.swift
+//  CookingRecipeApp
+//
+//  Created by Douglas Pfeifer on 18/09/21.
+//
+
+import Foundation
+
+struct Strings {
+    // MARK: Milk group
+    struct MilkGroup {
+        static let skimmedMilk = "Leite desnatado"
+        static let semiSkimmedMilk = "Leite semidesnatado"
+        static let regularMilk = "Leite integral"
+        static let curd = "Coalhada"
+        static let regularYogurt = "Iogurte natural"
+        static let fruitYogurt = "Iogurte de fruta"
+        static let minasCheese = "Queijo de minas"
+        static let regularCreamCheese = "Requeijão"
+        static let creamyCreamCheese = "Requeijão cremoso"
+        static let tofu = "Tofu"
+        static let blanketChicken = "Blanket frango"
+    }
+    
+    // MARK: Bread group
+    struct BreadGroup {
+        static let breadFully = "Pão de forma integral"
+        static let glutenFreeBread = "Pão sem glúten"
+        static let sirianBread = "Pão sírio médio integral - contém glúten"
+        static let lowCarbCake = "Bolo integral ou low carb"
+        static let cuscuz = "Cuscuz"
+        static let wholemealToast = "Torrada integral bauduco"
+        static let minasCheese = "Queijo de minas"
+        static let regularCreamCheese = "Requeijão"
+        static let creamyCreamCheese = "Requeijão cremoso"
+        static let tofu = "Tofu"
+        static let sweetPotatoTapioca = "Tapioca de batata doce"
+    }
+    
+    // MARK: Rice group
+    struct RiceGroup {
+        static let brownRice = "Arroz integral"
+        static let parabolizedRice = "Arroz parabolizado"
+        static let whiteRice = "Arroz branco"
+        static let englishMashedPotatoes = "Purê de batata inglesa"
+        static let mashedYams = "Purê de inhame"
+        static let yams = "Inhame"
+        static let angu = "Angu"
+        static let wholeNoodles = "Macarrão cozido integral"
+        static let wholePastaWithGarlicAndOil = "Macarrão ao alho e óleo (azeite) integral"
+        static let cornNoodles = "Macarrão de milho"
+        static let riceNoodles = "Macarrão de arroz"
+        static let englishPotato = "Batata inglesa"
+        static let sweetPotato = "Batata doce"
+        static let manioc = "Mandioca"
+    }
+    
+    // MARK: Nut group
+    struct NutGroup {
+        static let paraNut = "Castanha do pará"
+        static let cajuNut = "Castanha de caju"
+        static let pistachio = "Pistache"
+        static let peanut = "Amendoim"
+    }
+    
+    // MARK: Meat group
+    struct MeatGroup {
+        static let quinoa = "Quinua"
+        static let beef = "Bife"
+        static let mincedMeat = "Carne moída"
+        static let boiledEgg = "Ovo cozido"
+        static let quaillEgg = "Ovo de codorna"
+        static let roastChicken = "Frango assado"
+        static let cookedChicken = "Frango cozido"
+        static let chickenMeatballs = "Almondêga de frango"
+        static let meatballs = "Almôndega de carne"
+    }
+    
+    // MARK: Fruit group
+    struct FruitGroup {
+        static let pineapple = "Abacaxi"
+        static let avocado = "Abacate"
+        static let plum = "Ameixa"
+        static let prune = "Ameixa seca"
+        static let appleBanana = "Banana maçã"
+        static let nanicaBanana = "Banana nanica"
+        static let silverBanana = "Banana prata"
+        static let dryBanana = "Banana passa"
+        static let cajamanga = "Cajamanga"
+        static let cashew = "Cajú"
+        static let khaki = "Caqui"
+        static let damascus = "Damasco"
+        static let countsFruit = "Fruta do conde"
+        static let dryFig = "Figo seco"
+        static let jabuticaba = "Jabuticaba"
+        static let orange = "Laranja"
+        static let apple = "Maçã"
+        static let beautifulPapaya = "Mamão formosa"
+        static let mango = "Manga"
+        static let watermelon = "Melancia"
+        static let melon = "Melão"
+        static let tangerine = "Mexerica"
+        static let strawberry = "Morango"
+        static let pear = "Pêra"
+        static let peach = "Pêssego"
+        static let kiwi = "Kiwi"
+        static let starFruit = "Carambola"
+        static let grape = "Uva"
+        static let dryGrape = "Uva passa"
+        static let pineappleJuice = "Suco de abacaxi"
+        static let melonJuice = "Suco de melão"
+        static let acerolaJuice = "Suco de acerola"
+        static let guavaJuice = "Suco de goiaba"
+        static let cupuaçuJuice = "Suco de cupuaçu"
+    }
+    
+    // MARK: Bean group
+    struct BeanGroup {
+        static let bean = "Feijão"
+        static let lentil = "Lentilha"
+        static let boiledPea = "Ervilha cozida"
+        static let chickpea = "Grão de bico"
+    }
+    
+    // MARK: Cereal group
+    struct CerealGroup {
+        static let rolledOats = "Aveia em flocos"
+        static let oatmealFlour = "Farinha de aveia"
+        static let granola = "Granola"
+        static let quinoaFlour = "Farinha de quinoa"
+        static let greenBananaFlour = "Farinha de banana verde"
+        static let chiaFlour = "Farinha de chia"
+        static let grapeSeedFlour = "Farinha de semente de uva"
+    }
+}
