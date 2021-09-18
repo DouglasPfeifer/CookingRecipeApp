be503c2b30331b2a472932bb0fd85df3a840020e
Douglas <douglaspfeifer@gmail.com>
2021-09-18 17:57:02 -0300

    iOS initial commit

---
+++ iOS/CookingRecipeApp/CookingRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingRecipeAppApp.swift
+//  CookingRecipeApp
+//
+//  Created by Douglas Pfeifer on 18/09/21.
+//
+
+import SwiftUI
+
+@main
+struct CookingRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
